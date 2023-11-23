37c8be8c0190e3e4210c56299aaa4b3e60dd8c63
thithi250696 <145406781+thithi250696@users.noreply.github.com>
2023-11-23 14:56:05 +0900

    상품등록

---
+++ ToyConn_pro/src/main/java/controller/ProductService.java
@@ -0,0 +1,55 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Product;
+import model.ProductDAO;
+
+@WebServlet("/ProductService")
+public class ProductService extends HttpServlet {
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int p_num = Integer.parseInt( request.getParameter("p_num"));
+		String category = request.getParameter("category");
+		String p_quality = request.getParameter("p_quality");
+		String gender = request.getParameter("gender");
+		String image_file = request.getParameter("image_file");
+		String p_name = request.getParameter("p_name");
+		String p_content = request.getParameter("p_content");
+		int rent_price = Integer.parseInt( request.getParameter("rent_price"));
+		System.out.println("[ProductService]");
+		System.out.println("Product name: " + p_name);
+		System.out.println("Product description: " + p_content);
+		System.out.println("Rent price: " + rent_price);
+
+		Product vo = new Product(p_num, category, p_name, rent_price,p_quality,p_content,"대여가능","대여가능",gender, image_file);
+
+		int cnt = new ProductDAO().registerProduct(vo);
+		System.out.println("cnt : " + cnt);
+		if (cnt > 0) {
+			System.out.println("Register produt successfully");
+
+			request.setAttribute("p_name", p_name);
+
+		} else {
+			System.out.println("Register product fail");
+
+		}
+		 response.sendRedirect("main.jsp");
+		
+	}
+
+	
+
+		
+		
+		
+	}
+
+
--- ToyConn_pro/src/main/java/model/Product.java
+++ ToyConn_pro/src/main/java/model/Product.java
@@ -1,7 +1,5 @@
 package model;
 
-
-
 public class Product {
 	private int p_num;
 	 private String category;
@@ -13,5 +11,79 @@ public class Product {
 	String r_status = "대야가능";
 	private String gender;
 	private String image_file;
+	public Product(int p_num, String category, String p_name, int rent_price, String p_quality, String p_content,
+			String p_status, String r_status, String gender, String image_file) {
+		super();
+		this.p_num = p_num;
+		this.category = category;
+		this.p_name = p_name;
+		this.rent_price = rent_price;
+		this.p_quality = p_quality;
+		this.p_content = p_content;
+		this.p_status = p_status;
+		this.r_status = r_status;
+		this.gender = gender;
+		this.image_file = image_file;
+	}
+	public int getP_num() {
+		return p_num;
+	}
+	public void setP_num(int p_num) {
+		this.p_num = p_num;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getP_name() {
+		return p_name;
+	}
+	public void setP_name(String p_name) {
+		this.p_name = p_name;
+	}
+	public int getRent_price() {
+		return rent_price;
+	}
+	public void setRent_price(int rent_price) {
+		this.rent_price = rent_price;
+	}
+	public String getP_quality() {
+		return p_quality;
+	}
+	public void setP_quality(String p_quality) {
+		this.p_quality = p_quality;
+	}
+	public String getP_content() {
+		return p_content;
+	}
+	public void setP_content(String p_content) {
+		this.p_content = p_content;
+	}
+	public String getP_status() {
+		return p_status;
+	}
+	public void setP_status(String p_status) {
+		this.p_status = p_status;
+	}
+	public String getR_status() {
+		return r_status;
+	}
+	public void setR_status(String r_status) {
+		this.r_status = r_status;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getImage_file() {
+		return image_file;
+	}
+	public void setImage_file(String image_file) {
+		this.image_file = image_file;
+	}
 	
 }
