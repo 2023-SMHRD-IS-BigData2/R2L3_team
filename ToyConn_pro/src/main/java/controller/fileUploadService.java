1e05201ee8d59d5470e2ae824d6c2c84a1042c68
smhrd <smhrd@220.71.97.187>
2023-11-28 18:05:13 +0900

    수정

--- ToyConn_pro/src/main/java/controller/fileUploadService.java
+++ ToyConn_pro/src/main/java/controller/fileUploadService.java
@@ -40,10 +40,8 @@ public class fileUploadService extends HttpServlet {
 		HttpSession session = request.getSession();
 		String id = (String)session.getAttribute("id");
 		String cate = multi.getParameter("cate");
-		String gender = "남여아완구";
-//				multi.getParameter("gender");
-		String qual = "중";
-//				multi.getParameter("qual");
+		String gender = multi.getParameter("gender");
+		String qual = multi.getParameter("qual");
 		String filename = multi.getFilesystemName("productImage");
 		String p_name = multi.getParameter("p_name");
 		String p_content = multi.getParameter("p_content");
